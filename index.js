10e5a7c5af5b1904c79f5f41e6b4196a1f81d236
RichS BiachS <72455727+RichsBiachs@users.noreply.github.com>
2020-11-15 23:23:42 +0400

    Update index.js

--- index.js
+++ index.js
@@ -197,7 +197,7 @@ client.on('guildMemberAdd', member => {
     .setTitle(`Привет ${member.displayName}!`)
     .setColor('#bbff00')
     .setDescription(`Добро пожаловать на ${guild.name}. Присаживайся и отдыхай от суеты. Нужна помощь? Напиши ~help и вам будет предоставлена помощь!`)
-    .setFooter(ID:${member.id} Зарегистрировался: ${member.user.createdAt}, "https://i.imgur.com/FJn0EDJ.png%22%22")
+    .setFooter(`ID:${member.id} Зарегистрировался: ${member.user.createdAt}`, "https://i.imgur.com/FJn0EDJ.png%22%22")
     .setImage("https://i.imgur.com/5GQsVsX.png%22%22")
     .setThumbnail("https://i.imgur.com/I7YKt8M.png")
     client.channels.cache.get('756211425038565386').send(embed);
